f509565de41832eb040c99d30bc41d152cd8e858
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-04-23 11:58:48 -0700

    Bootstrap for simulation
    The following patch adds the necessary bootstrapping for the simulation.
    
    As the saying goes: "everything is a nail when you have a hammer".
    Pytest is usually used for testing, but it can be honestly used as a runner
    for almost everything. The scoping and lifetime of the fixtures,
    combined with the testcase isolation and parametrization, makes
    writing a simulation a breeze.
    
    One pending thing is to better visualize the values and hopefully
    we see some nice trends.

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,9 +1,13 @@
 import os
+import random
 import time
+from collections import defaultdict
 
+import pandas as pd
 import pytest
 
 from sqlitecache.cache import (
+    BaseCache,
     CacheSettings,
     DiskStorage,
     HybridCache,
@@ -168,3 +172,98 @@ class TestHybridCache:
     def test_hybrid_put(self, hybrid_cache):
         hybrid_cache.put("key", "value")
         assert hybrid_cache.get("key") == "value"
+
+
+p_values = [100, 200, 300, 500]
+q_values = [100, 200, 300, 500]
+p_ids = [f"p={p}" for p in p_values]
+q_ids = [f"q={q}" for q in q_values]
+n_request_values = [100, 200, 300, 500]
+n_request_ids = [f"n_requests={n_requests}" for n_requests in n_request_values]
+
+
+@pytest.mark.simulation
+class TestSimulation:
+    @pytest.fixture(scope="class")
+    def recorder(self):
+        # Use the recorder such that we can collect the session stats
+        _recorder = defaultdict(list)
+        yield _recorder
+        # convert the stats to multiple dataframes, depending on they key
+        for key, values in _recorder.items():
+            df = pd.DataFrame(values)
+            df.to_csv(f"results/{key}.csv", index=False)
+            print(f"Stats for {key}:")
+            print(df.describe())
+
+    @pytest.fixture
+    def cache(self, request, p, q, db, disk_storage, n_requests, recorder):
+        _cache = None
+        if request.param == "lru_cache":
+            size_settings = CacheSettings(max_size_in_bytes=p)
+            _cache = LRUCache(db=db, storage=disk_storage, settings=size_settings)
+        elif request.param == "lfu_cache_no_ttl":
+            size_settings = CacheSettings(max_size_in_bytes=q)
+            _cache = LFUCache(db=db, storage=disk_storage, settings=size_settings)
+        else:
+            raise ValueError("Invalid cache type")
+        yield _cache
+        # collection of stats
+        hit_rate, miss_rate = _cache.get_rates()
+        recorder[request.param].append(
+            {
+                "size": p if request.param == "lru_cache" else q,
+                "n_requests": n_requests,
+                "hit_rate": hit_rate,
+                "miss_rate": miss_rate,
+            }
+        )
+        print(f"Hit rate: {hit_rate}, Miss rate: {miss_rate}")
+
+    @pytest.mark.parametrize("p", p_values, ids=p_ids)
+    @pytest.mark.parametrize("q", q_values, ids=q_ids)
+    @pytest.mark.parametrize("cache", ["lru_cache", "lfu_cache_no_ttl"], indirect=True)
+    @pytest.mark.parametrize("n_requests", n_request_values, ids=n_request_ids)
+    def test_simulation(self, p, q, cache: BaseCache, n_requests):
+        """This is basically a simulation of doing random requests to the cache.
+        Because our cache is "key" oriented rather than value oriented,
+        we can insert the same value under random keys.
+
+        The probability of reading and writing to the cache is 50% each.
+        """
+
+        def get_random_key():
+            return f"key{random.randint(1, n_requests)}"
+
+        inserted_keys_set = set()
+        inserted_keys_lst = []
+
+        # Penalty on memory, but easy lookup and avoid
+        # overhead of converting the set into a list
+        def perform_insert():
+            key = get_random_key()
+            value = "value"
+            if key not in inserted_keys_set:
+                inserted_keys_set.add(key)
+                inserted_keys_lst.append(key)
+            cache.put(key, value)
+
+        def perform_read():
+            if not inserted_keys_lst:
+                return
+            key = random.choice(inserted_keys_lst)
+            value = cache.get(key)
+            if value is None:
+                # It is possible to have a hit miss,
+                # but we need to update these two such that we can
+                # always pick a read from the current values
+                inserted_keys_set.remove(key)
+                inserted_keys_lst.remove(key)
+
+        for _ in range(n_requests):
+            prob = random.random()
+            if prob < 0.5:
+                # Write to the cache
+                perform_insert()
+            else:
+                perform_read()
