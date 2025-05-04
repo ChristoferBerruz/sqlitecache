b86a1508e134f38096a8a2c30837c7dada90bfd6
Christofer Berruz Chungata <cberruzc@my.bridgeport.edu>
2025-05-03 20:54:57 -0700

    Handy flag for simulation
    This commit adds a simulation flag in order to
    streamline the way we run simulation testcases.

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,12 +12,35 @@ def pytest_configure(config):
 
 def pytest_addoption(parser):
     parser.addoption("--n-requests", action="store", type=int, default=100)
+    parser.addoption("--simulation", action="store_true", default=False)
 
 
 @pytest.fixture
 def n_requests(request):
     """Fixture to get the number of requests from the command line."""
     yield request.config.getoption("--n-requests")
+
+
+def pytest_collection_modifyitems(config, items):
+    """Modifying collection for better streamlining of simulation tests."""
+    simulation_okay = config.getoption("--simulation")
+    kept_items = []
+    deselected_items = []
+    for item in items:
+        is_simulation_marked = item.get_closest_marker("simulation")
+        # simulation marked tests are only kept if --simulation is given
+        # regular tests are kept only when no --simulation is given
+        if is_simulation_marked and not simulation_okay:
+            deselected_items.append(item)
+        elif is_simulation_marked and simulation_okay:
+            kept_items.append(item)
+        elif not is_simulation_marked and not simulation_okay:
+            kept_items.append(item)
+        else:
+            deselected_items.append(item)
+
+    config.hook.pytest_deselected(items=deselected_items)
+    items[:] = kept_items
 
 
 @pytest.fixture(autouse=True, scope="session")
